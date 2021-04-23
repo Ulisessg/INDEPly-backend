93a54751f64d0660f1cafb7734cef3c0fe278e5f
ulisessg <ukisessg@gmail.com>
2021-04-23 04:15:10 -0500

    [ADD] Express added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('TS added');
+import Express from 'express';
+
+const app = Express();
+const port = process.env.PORT || 8080;
+
+app.use('/', (req, res) => {
+  res.json({ done: true });
+});
+
+app.listen(port, () => {
+  console.log(`Server running in http://localhost:${port}`);
+});
